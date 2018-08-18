3677a0d4440ccea64d04a4c88772ad47f3fbeb25
xFelipe <felipegdmito@Gmail.com>
2018-08-18 17:40:54 -0300

    antes da revisão

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+    User.create(email: "user-numero#{n}@email.com", password: '123321')
+end

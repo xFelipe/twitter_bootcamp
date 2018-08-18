15580a7cfe16216adf747d168aa11448a7e31581
xFelipe <felipegdmito@Gmail.com>
2018-08-18 16:47:35 -0300

    tweets complementados

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:'pages#home'
-  resources :tweets
+  
+  resources :tweets, only: :create
+  resources :users, only: [:index, :show]
 end
